6347d379e4c8bb2636ce6af9626d428a557e32d1
Sven Schliesing <s.schliesing@ndr.de>
2017-08-23 09:28:20 +0200

    NDRB-2543: dict mit 'xml' und 'successful' zurückgeben

--- sophora/importer.py
+++ sophora/importer.py
@@ -14,18 +14,23 @@ def import_xml(soap_config, xml):
         client = Client(soap_config['url'], transport=Transport(session=session))
 
         if soap_config['instance']:
-            result = client.service.importXmlToInstance(xml, soap_config['instance'])
+            soap_result = client.service.importXmlToInstance(xml, soap_config['instance'])
         else:
-            result = client.service.importXml(xml)
+            soap_result = client.service.importXml(xml)
 
-        result_xml = BeautifulSoup(result, 'xml')
+        result_xml = BeautifulSoup(soap_result, 'xml')
+        result = {
+            'xml': result_xml
+        }
 
         if result_xml.importInformation['successful'] == "true":
             logger.info("Imported Sophora-XML for '%s' to '%s'", result_xml.sophoraId.text, soap_config['url'])
+            result['successful'] = True
         else:
             logger.fatal("SOAP Import failed: %s", result_xml.importInformation.errorText.text)
+            result['successful'] = False
 
-        return result_xml
+        return result
 
     except Exception:
         logger.fatal("SOAP error: %s", soap_config['url'], exc_info=True)
